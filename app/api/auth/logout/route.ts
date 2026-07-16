dc30af7bed6c374c08ebdc61be99cc1d6d759c6b
duyenntk-cmd <duyenntk@apero.vn>
2026-07-17 00:39:48 +0700

    Remove login system

--- app/api/auth/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!;
-  const response = NextResponse.redirect(`${baseUrl}/login`);
-  response.cookies.delete("app_session");
-  return response;
-}
